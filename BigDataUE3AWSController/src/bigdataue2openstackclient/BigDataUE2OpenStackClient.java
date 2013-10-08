5829d68eeedb893b2ee7b27b04ff1356f2459cee
Nepetalactone <tobias.meusburger@students.fhv.at>
2013-10-08 19:35:15 +0200

    Methodenaufruf nun asynchron
    Signed-off-by: Nepetalactone <tobias.meusburger@students.fhv.at>

--- BigDataUE3AWSController/src/bigdataue2openstackclient/BigDataUE2OpenStackClient.java
+++ BigDataUE3AWSController/src/bigdataue2openstackclient/BigDataUE2OpenStackClient.java
@@ -7,6 +7,8 @@ package bigdataue2openstackclient;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,6 +21,9 @@ public class BigDataUE2OpenStackClient {
     /**
      * @param args the command line arguments
      */
+    
+    int number;
+    
     public static void main(String[] args) {
         System.out.print("Wie oft soll der Monte Carlo Algorithmus ausgeführt werden?");
        
@@ -32,14 +37,51 @@ public class BigDataUE2OpenStackClient {
         }
         
         try{
-            int number = Integer.parseInt(numberOfThrows);
-            double numberFromFirstService = calculatePi(number);
-            double numberFromSecondService = calculatePi_1(number);
-            double numberFromThirdService = calculatePi_2(number);
-            System.out.print("Erster Service berechnete für Pi " + numberFromFirstService + "\n");
-            System.out.print("Zweiter Service berechnete für Pi " + numberFromSecondService + "\n");
-            System.out.print("Dritter Service berechnete für Pi " + numberFromThirdService + "\n");
-            System.out.print("Pi: " + (numberFromFirstService + numberFromSecondService + numberFromThirdService)/3);
+            final int number = Integer.parseInt(numberOfThrows);
+            
+            final Double[] piValues = {0.0, 0.0, 0.0 };
+        
+            Thread t1 = new Thread(){
+                @Override
+                public void run(){
+                    piValues[0] = new Double(calculatePi(number));
+                }
+            };
+            
+            Thread t2 = new Thread(){
+                @Override
+                public void run(){
+                    piValues[1] = new Double(calculatePi_1(number));
+                }
+            };
+            
+            Thread t3 = new Thread(){
+                @Override
+                public void run(){
+                    piValues[2] = new Double(calculatePi_2(number));
+                }
+            };
+        
+            t1.start();
+            t2.start();
+            t3.start();
+        
+            float i = 0;
+            
+            while((piValues[0] == 0) || (piValues[1] == 0) || (piValues[2] == 0) ){
+                if (i == 10000000){
+                    System.out.println("waiting"); //Springt aus unbekannten Gründen nicht aus der Schleife wenn nicht etwas in der Schleife getan wurde
+                    i = 0;
+                } else {
+                    i++;
+                }
+            }
+            
+            System.out.println("Ergebnisse der einzelnen Services:\nErster Service: " + piValues[0] 
+                    + "\nZweiterService: " + piValues[1] 
+                    + "\nDritterService: " + piValues[2] + "\n");
+            System.out.println("Pi beträgt: " + (piValues[0] + piValues[0] + piValues[0]) / 3);
+            
         }catch(NumberFormatException e)
         {
             System.out.print("Anzahl ist keine gültige Nummer");
@@ -63,8 +105,6 @@ public class BigDataUE2OpenStackClient {
         bigdataue2package3.PiCalculator port = service.getPiCalculatorPort();
         return port.calculatePi(numberOfThrows);
     }    
-        
-    
 }
 
     
